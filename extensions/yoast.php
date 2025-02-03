29c56e61ba4f8bf3c322ddf3067e042425c1e735
Vic Lobins <vic.l@pomegranate.co.uk>
2025-02-03 15:01:54 +0000

    added yoast redirects

--- extensions/yoast.php
+++ extensions/yoast.php
@@ -18,9 +18,21 @@ class NextPressYoastExtension
     {
         if (!function_exists('YoastSEO')) return $post;
         $meta_helper = YoastSEO()->classes->get(Yoast\WP\SEO\Surfaces\Meta_Surface::class);
-        $meta = $meta_helper->for_post(is_object($post) ? $post->ID : $post['id']);
+        $post_id = is_object($post) ? $post->ID : $post['id'];
+        $meta = $meta_helper->for_post($post_id);
         if (!$meta) return $post;
         $post['yoastHeadJSON'] = $meta->get_head()->json;
+
+        // Check for redirects.
+        $redirects_json = get_option('wpseo-premium-redirects-base');
+        $permalink = get_permalink($post_id);
+        if ($redirects_json && $permalink) {
+            foreach ($redirects_json as $redirect) {
+                if (strpos($permalink, $redirect['origin']) !== false) {
+                    $post['yoastHeadJSON']['redirect'] = $redirect['url'];
+                }
+            }
+        }
         return $post;
     }
 }
