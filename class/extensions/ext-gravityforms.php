5af76a3b4e73e3682238b86ab62b4f340fad77ab
Vic Lobins <vic.l@pomegranate.co.uk>
2025-11-19 17:56:32 +0000

    ext gf form data fixed for multiple forms

--- class/extensions/ext-gravityforms.php
+++ class/extensions/ext-gravityforms.php
@@ -15,9 +15,26 @@ class Ext_GravityForms {
   }
 
   public function include_gf_data( $block_data ) {
-    if ( ! isset( $block_data['gravity_form'] ) ) return $block_data;
+    // Early exit.
     if ( ! class_exists( 'GFAPI' ) ) return $block_data;
-    $block_data['gfData'] = \GFAPI::get_form( str_replace( 'form_id_', '', $block_data['gravity_form'] ) );
+
+    // Check if contains key: gravity_form
+    $is_gf = isset( $block_data['gravity_form'] );
+    if ( $is_gf ) {
+      $block_data['gfData'] = \GFAPI::get_form( str_replace( 'form_id_', '', $block_data['gravity_form'] ) );
+      return $block_data;
+    } else {
+      // Check if any fields are keyed with 'gravity' and 'form'.
+      $keys = array_keys( $block_data );
+      foreach ( $keys as $key ) {
+        if ( strpos( $key, 'gravity' ) !== false && strpos( $key, 'form' ) !== false ) {
+          $data_key = "${key}_data";
+          $block_data[ $data_key ] = \GFAPI::get_form( str_replace( 'form_id_', '', $block_data[ $key ] ) );
+          return $block_data;
+        }
+      }
+    }
+
     return $block_data;
   }
 }
