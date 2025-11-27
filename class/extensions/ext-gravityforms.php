8d82c63aae726a60b3a8d48242128fa3f479d0e0
Vic Lobins <vic.l@pomegranate.co.uk>
2025-11-27 17:46:27 +0000

    acf gf fix

--- class/extensions/ext-gravityforms.php
+++ class/extensions/ext-gravityforms.php
@@ -12,6 +12,7 @@ defined('ABSPATH') or die('You do not have access to this file');
 class Ext_GravityForms {
   public function __construct() {
     add_filter( 'nextpress_block_data', [ $this, 'include_gf_data' ], 10, 2 );
+    add_filter( 'nextpress_post_object', [ $this, 'include_gf_post_data' ], 10, 1 );
   }
 
   public function include_gf_data( $block_data ) {
@@ -36,5 +37,30 @@ class Ext_GravityForms {
     }
 
     return $block_data;
+  }
+
+  public function include_gf_post_data( $post ) {
+    // Early exit.
+    if ( ! class_exists( 'GFAPI' ) ) return $post;
+
+    // Check if contains key: gravity_form
+    $is_gf = isset( $post['acf_data']['gravity_form'] );
+    if ( $is_gf ) {
+      $post['acf_data']['gfData'] = \GFAPI::get_form( $post['acf_data']['gravity_form'] );
+      return $post;
+    } elseif ( isset( $post['acf_data'] ) && is_array( $post['acf_data'] ) ) {
+      // Check if any fields are keyed with 'gravity' and 'form'.
+      $keys = array_keys( $post['acf_data'] );
+      foreach ( $keys as $key ) {
+        if ( strpos( $key, 'gravity' ) !== false && strpos( $key, 'form' ) !== false ) {
+          $data_key = "${key}_data";
+          $post['acf_data'][ $data_key ] = \GFAPI::get_form( $post['acf_data'][ $key ] );
+          return $post;
+        }
+      }
+    }
+
+
+    return $post;
   }
 }
